dc8857c4eaa6fa755afdd421383704ab3ec64e3c
HongJiHyun <fnvl7855@naver.com>
2023-01-18 01:35:23 +0900

    Test

--- config/Protobuf/bin/Match/GenPackets.bat
+++ config/Protobuf/bin/Match/GenPackets.bat
@@ -1,12 +1,22 @@
 pushd %~dp0
 
 protoc.exe -I=./ --cpp_out=./ ./ProtocolMatch.proto
+protoc.exe -I=./ --cpp_out=./ ./ProtocolServer.proto
 
 XCOPY /Y ProtocolMatch.pb.h "../../../../src/DummyClient"
 XCOPY /Y ProtocolMatch.pb.cc "../../../../src/DummyClient"
+
 XCOPY /Y ProtocolMatch.pb.h "../../../../src/ServerMatch"
 XCOPY /Y ProtocolMatch.pb.cc "../../../../src/ServerMatch"
 
+XCOPY /Y ProtocolMatch.pb.h "../../../../src/ServerGame"
+XCOPY /Y ProtocolMatch.pb.cc "../../../../src/ServerGame"
+
+XCOPY /Y ProtocolServer.pb.h "../../../../src/ServerMatch"
+XCOPY /Y ProtocolServer.pb.cc "../../../../src/ServerMatch"
+
+XCOPY /Y ProtocolServer.pb.h "../../../../src/ServerGame"
+XCOPY /Y ProtocolServer.pb.cc "../../../../src/ServerGame"
 IF ERRORLEVEL 1 PAUSE
 
 DEL /Q /F *.pb.h
