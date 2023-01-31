7e797af70ac97fac82d307dd62f1803c12e759ef
MinKwan Kim <minkwan_k@naver.com>
2023-01-31 23:16:41 +0900

    controller modifying

--- src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/MyPlayerController.cs
+++ src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -13,15 +13,24 @@ public class MyPlayerController : PlayerController
 
     }
 
-   
+    void Update()
+    {
+        GetInput();
+        UpdateVec();
+        UpdateMove();
+    }
 
     protected override void GetInput()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
+        //Debug.Log("h" + h + "v" + v);
         if (h == 0.0 && v == 0.0)
+        {
             _state = PlayerState.Idle;
+            moveVec = new Vector3(0, 0, 0);
+        }
 
         else
             _state = PlayerState.Moving;
--- src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/PlayerController.cs
+++ src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/PlayerController.cs
@@ -9,9 +9,10 @@ public class PlayerController : MonoBehaviour
 {
     public Int64 id { get; set; }
     [SerializeField]
-    public float speed = 5.0f;
+    public float speed = 10.0f;
     protected float h, v;
 
+    protected Vector3 prevVec;
     protected Vector3 moveVec;
     protected Animator _animator;
     protected Rigidbody _rigidbody;
@@ -44,7 +45,7 @@ public class PlayerController : MonoBehaviour
                 return;
 
             _playerInfo = value;
-            UpdateMove();
+            //UpdateMove();
         }
     }
 
@@ -56,8 +57,10 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        GetInput();
+
+        UpdateVec();
         UpdateMove();
+
     }
 
 
@@ -65,48 +68,50 @@ public class PlayerController : MonoBehaviour
     protected virtual void Init()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        prevVec = transform.position;
     }
 
-    /*
-	protected virtual void UpdateController()
-	{
-		switch (State)
-		{
-			case PlayerState.Idle:
-				GetDirInput();
-				break;
-			case PlayerState.Moving:
-				GetDirInput();
-				break;
-		}
-		
-		UpdateController();
-	}
-    */
+    
 
     // 키보드 입력
     protected virtual void GetInput()
     {
-        
-            
+
+      
     }
 
-    void UpdateMove()
+    protected Vector3 UpdateVec()
     {
+
         if (_state == PlayerState.Moving)
         {
             //벡터 값을 1로 보정해줌 
-            moveVec = new Vector3(h, 0, v).normalized;
+            //moveVec = new Vector3(h, 0, v).normalized;
 
-            transform.position += moveVec * speed * Time.deltaTime;
+            moveVec = new Vector3(h, 0, v);
+            moveVec = transform.position * speed * Time.deltaTime; 
 
-            transform.LookAt(transform.position + moveVec);
+            Debug.Log("UpdateVec : moveVec : " + moveVec);
+
+            //transform.position += moveVec * speed * Time.deltaTime;
+            //transform.LookAt(transform.position + moveVec);
+
+            return moveVec;
         }
-    
-       
+        else
+            return prevVec;       
     }
 
-  
+    protected void UpdateMove()
+    {
+        Debug.Log("UpdateMove");
+        transform.position += moveVec * speed * Time.deltaTime;
+        transform.LookAt(transform.position + moveVec);
+
+        prevVec = transform.position;
+    }
+
+
 
 
 }
