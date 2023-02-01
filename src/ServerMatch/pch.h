3d5424547c69a72de8fc8d8814cc70823a601166
LimTurtle <pigwoo98@gmail.com>
2023-02-01 13:47:59 +0900

    hiredis Library 추가

--- src/ServerMatch/pch.h
+++ src/ServerMatch/pch.h
@@ -5,9 +5,11 @@
 #ifdef _DEBUG
 #pragma comment(lib, "ServerCore\\Debug\\ServerCore.lib")
 #pragma comment(lib, "Protobuf\\Debug\\libprotobufd.lib")
+#pragma comment(lib, "hiredis\\Debug\\hiredisd.lib")
 #else
 #pragma comment(lib, "ServerCore\\Release\\ServerCore.lib")
 #pragma comment(lib, "Protobuf\\Release\\libprotobufd.lib")
+#pragma comment(lib, "hiredis\\Release\\hiredis.lib")
 #endif
 
 #include <CorePch.h>
