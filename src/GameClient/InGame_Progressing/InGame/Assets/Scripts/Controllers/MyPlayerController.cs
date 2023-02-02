a104e02e03d3b20109dfc1dfc3bc273fa6b4a268
unknown <minkwan_k@naver.com>
2023-02-02 12:21:21 +0900

    server commucation, can select communication mode

--- src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/MyPlayerController.cs
+++ src/GameClient/InGame_Progressing/InGame/Assets/Scripts/Controllers/MyPlayerController.cs
@@ -7,11 +7,17 @@ using static Define;
 public class MyPlayerController : PlayerController
 {
 
+    public  bool serverCommunication = false;
 
     protected override void Init()
     {
         base.Init();
     }
+    
+
+
+    
+
 
     protected override void UpdateController()
     {
@@ -27,13 +33,13 @@ public class MyPlayerController : PlayerController
         base.UpdateController();
     }
 
-
     void GetInput()
     {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
     }
+
 
     protected override void UpdateIdle()
     {
@@ -45,6 +51,100 @@ public class MyPlayerController : PlayerController
         }
     }
 
+    protected override void UpdateMoving()
+    {
+
+        //서버가 좌표를 보내주면 이동하는 형식
+        if (serverCommunication)
+        {
+            PlayerState prevState = State;
+            Vector3 prevVec = transform.position;
+
+
+            Vector3 moveVec = new Vector3(h, 0, v);
+            moveVec = transform.position + (moveVec * speed * Time.deltaTime);
+            //moveVec += transform.position * speed * Time.deltaTime;
+
+
+
+            if (prevState != State || prevVec != moveVec)
+            {
+
+                Move playerMove = new Move()
+                {
+                    Id = id,
+                    Position = new Vector { X = moveVec.x, Y = moveVec.y, Z = moveVec.z },
+                    Rotation = new Vector { X = moveVec.x, Y = moveVec.y, Z = moveVec.z },
+
+                };
+
+                Managers.Network.Send(playerMove, INGAME.PlayerMove);
+
+            }
+
+            if (playerInfo.Position.X == prevVec.x && playerInfo.Position.Z == prevVec.z)
+            {
+                State = PlayerState.Idle;
+                return;
+            }
+            else
+            {
+                State = PlayerState.Moving;
+                moveVec = new Vector3(playerInfo.Position.X, playerInfo.Position.Y, playerInfo.Position.Z);
+                transform.position = moveVec;
+
+                Debug.Log("Player:UpdateMoving : moveVec    " + moveVec + "State :" + State);
+
+
+            }
+        }
+        //내가 이동하고 좌표를 보내는 형식
+        else
+        {
+            PlayerState prevState = State;
+            Vector3 prevVec = transform.position;
+
+            if (h == 0 && v == 0)
+            {
+                State = PlayerState.Idle;
+                return;
+            }
+
+            Vector3 moveVec = new Vector3(h, 0, v);
+            transform.position += moveVec * speed * Time.deltaTime;
+
+            if (prevState != State || prevVec != transform.position)
+            {
+
+                Move playerMove = new Move()
+                {
+                    Id = id,
+                    Position = new Vector { X = transform.position.x, Y = transform.position.y, Z = transform.position.z },
+                    Rotation = new Vector { X = transform.rotation.x, Y = transform.rotation.y, Z = transform.rotation.z },
+
+                };
+
+                Managers.Network.Send(playerMove, INGAME.PlayerMove);
+
+            }
+        }
+     
+    }
+
+}
+
+
+    /*
+   
+    protected override void UpdateIdle()
+    {
+        if (h != 0 || v != 0)
+        {
+
+            State = PlayerState.Moving;
+            return;
+        }
+    }
 
 
     protected override void UpdateMoving()
@@ -78,5 +178,51 @@ public class MyPlayerController : PlayerController
 
     }
 
+    
+    protected override void UpdateMoving()
+       {
+        PlayerState prevState = State;
+        Vector3 prevVec = transform.position;
 
-}
+      
+        Vector3 moveVec = new Vector3(h, 0, v);
+        moveVec = transform.position + (moveVec*speed*Time.deltaTime);
+        //moveVec += transform.position * speed * Time.deltaTime;
+
+      
+
+        if (prevState != State || prevVec != moveVec)
+        {
+
+            Move playerMove = new Move()
+            {
+                Id = id,
+                Position = new Vector { X = moveVec.x, Y = moveVec.y, Z = moveVec.z },
+                Rotation = new Vector { X = moveVec.x, Y = moveVec.y, Z = moveVec.z },
+
+            };
+
+            Managers.Network.Send(playerMove,INGAME.PlayerMove);
+
+        }
+
+        if (playerInfo.Position.X == prevVec.x && playerInfo.Position.Z == prevVec.z)
+        {
+            State = PlayerState.Idle;
+            return;
+        }
+        else
+        {
+            State = PlayerState.Moving;
+            moveVec = new Vector3(playerInfo.Position.X, playerInfo.Position.Y, playerInfo.Position.Z);
+            transform.position = moveVec;
+
+            Debug.Log("Player:UpdateMoving : moveVec    " + moveVec + "State :" + State);
+
+           
+        }
+
+    */
+
+
+
